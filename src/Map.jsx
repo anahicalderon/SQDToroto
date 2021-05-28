16fe9b622e663924574d317498b41e97aabc8e47
Karen Tapia <75274720+Kar3n1na@users.noreply.github.com>
2021-05-28 15:59:24 -0500

    este repositorio tiene instalado react y mapbox

---
+++ src/Map.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 18.4442,
+    longitude: -89.1036,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoiZWxpemFiZXRoZ2giLCJhIjoiY2twOHBkaHNvMDN1MjJvcDR6aGhpM2h2ayJ9.U3EK7VZc-urMFKxXy83cpQ"
+        mapStyle="mapbox://styles/elizabethgh/ckp8r5cxj28lq18p5ud5gwhy4"
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        marker here
+      </ReactMapGL>
+    </div>
+  );
+}
