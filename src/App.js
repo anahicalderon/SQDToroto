caccff7f1d9d2e2dbcadcd5d37d1ae7139f3266c
jazmin <tegan01sara12@gmail.com>
2021-05-31 19:43:16 -0500

    Capas de los dos proyectos ajustes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Map from "./Map";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import LagunaOm from "./LagunaOm";
-import NuevoBacal from "./NuevoBecal";
+import NuevoBacal from "./Nuevo Becal";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
@@ -16,10 +16,10 @@ function App() {
           <Route exact path="/">
             <Map />
           </Route>
-          <Route exact path="/lagunaom">
+          <Route exact path="/LagunaOm">
             <LagunaOm />
           </Route>
-          <Route exact path="/nuevobecal">
+          <Route exact path="/Nuevo Becal">
             <NuevoBacal />
           </Route>
         </Switch>
